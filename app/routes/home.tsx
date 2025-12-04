d84120d91e3dd289d4465222358461fb0fab4ee1
dmshvetsov <shvetsovdm@gmail.com>
2025-12-04 22:04:56 +0400

    setup preview for farcaster mini-app

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
+    { title: 'On-Chain Wrapped' },
+    { name: 'description', content: 'On-Chain Wrapped base and farcaster mini-app' },
   ];
 }
 
