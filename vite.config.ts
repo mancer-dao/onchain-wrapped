6ce8ec1bb35567a4d2b3eb6b3ec0769abe226071
dmshvetsov <shvetsovdm@gmail.com>
2025-12-26 11:33:34 +0200

    allow to use tunnel in local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   clearScreen: false,
   plugins: [tailwindcss(), react(), tsconfigPaths()],
+  server: {
+    allowedHosts: [
+      "farcaster-oracle-tunnel.magicdima.xyz",
+      "localhost-5173.magicdima.xyz",
+    ],
+  },
 });
